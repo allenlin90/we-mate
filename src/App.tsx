7a8a601d1a9471ca0e60a102df241d7f4f61ae0c
allenlin <ht016746@gmail.com>
2024-04-26 07:49:52 +0700

    chore: meet page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from '@/pages/home';
+import Meet from '@/pages/meet';
 import Layout from '@/components/layout';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />;
       <Route element={<Layout />}>
-        <Route path='/meet' element={<div>Meet</div>} />
+        <Route path='/meet' element={<Meet />} />
       </Route>
     </Routes>
   );
