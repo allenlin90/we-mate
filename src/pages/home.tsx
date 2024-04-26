d4607fe5f7118e835f293c92053b68cfe5cf81c2
allenlin <ht016746@gmail.com>
2024-04-26 08:47:40 +0700

    chore: wrap logo in container

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,9 @@ import Logo from '@/components/logo';
 function Home() {
   return (
     <div className='inline-flex flex-col justify-center align-items gap-8 p-4 w-full text-center h-dvh'>
-      <Logo />
+      <div className='w-full max-w-[80vw] sm:max-w-xs mx-auto'>
+        <Logo />
+      </div>
       <Link to='/meet'>
         <Button className='capitalize bg-mate-pink-dark'>meet a mate</Button>
       </Link>
