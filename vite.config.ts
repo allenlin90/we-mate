bb0c0090ac02b57e19756742d72c42f542c68c63
allenlin <ht016746@gmail.com>
2024-04-27 10:52:21 +0700

    chore: add vite pwa and asset cache pattern

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default () => {
   const PORT = +(process.env.PORT || 3000);
 
   return defineConfig({
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}'], // cache assets by file types
+        },
+      }),
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
