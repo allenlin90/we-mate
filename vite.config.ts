f1d1d7e350430d34de1f8af4b9757f272696dd92
allenlin <ht016746@gmail.com>
2024-04-27 11:30:55 +0700

    build: pwa init setup

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,25 @@ export default () => {
         workbox: {
           globPatterns: ['**/*.{js,css,html,ico,png,svg}'], // cache assets by file types
         },
+        includeAssets: ['favicon.ico', 'icons/apple-touch-icon.png'],
+        manifest: {
+          name: 'We Mate',
+          short_name: 'WeMate',
+          description: 'We Meet A Mate',
+          theme_color: '#ff5931',
+          icons: [
+            {
+              src: 'icons/pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'icons/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        },
       }),
     ],
     resolve: {
